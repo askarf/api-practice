cbd96a4589c0a67c401021fe934d966e23a0d71f
askarf <ayalakarf@gmail.com>
2023-10-24 14:22:52 -0400

    added if statement to ask user which product to print

--- product_app.rb
+++ product_app.rb
@@ -1,8 +1,18 @@
 require "http"
 
-first_product = HTTP.get("http://localhost:3000/first_product.json")
-last_product = HTTP.get("http://localhost:3000/last_product.json")
-all_products = HTTP.get("http://localhost:3000/all_products.json")
+first_product = HTTP.get("http://localhost:3000/one_product.json").parse
+last_product = HTTP.get("http://localhost:3000/last_product.json").parse
+all_products = HTTP.get("http://localhost:3000/all_products.json").parse
 
-p "would you like"
+p "Welcome to my consignment shop!"
+p "would you like to see the first, last or all products?"
 input = gets.chomp
+if input == "first"
+  pp first_product
+elsif input == "last"
+  pp last_product
+elsif input == "all"
+  pp all_products
+else
+  p "im sorry the options are first, last or all. Please try again"
+end

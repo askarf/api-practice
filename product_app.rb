dcaa0901ae783578a84e03ee8a42bf9808eb392a
askarf <ayalakarf@gmail.com>
2023-10-25 14:01:47 -0400

    added link to specific product depending on user input

--- product_app.rb
+++ product_app.rb
@@ -1,18 +1,22 @@
 require "http"
+require "tty-table"
 
-first_product = HTTP.get("http://localhost:3000/one_product.json").parse
-last_product = HTTP.get("http://localhost:3000/last_product.json").parse
-all_products = HTTP.get("http://localhost:3000/all_products.json").parse
+all_products = HTTP.get("http://localhost:3000/products.json").parse
 
 p "Welcome to my consignment shop!"
-p "would you like to see the first, last or all products?"
+p "would you like to see a specific item or a display of all of them??"
 input = gets.chomp
-if input == "first"
-  pp first_product
-elsif input == "last"
-  pp last_product
+if input == "specific"
+  puts "which id would you like to see?"
+  input_id = gets.chomp.to_i
+  specific_product = HTTP.get("http://localhost:3000/products/#{input_id}.json").parse
+  pp specific_product
 elsif input == "all"
   pp all_products
 else
-  p "im sorry the options are first, last or all. Please try again"
+  p "im sorry the options are specific or all, please try again"
 end
+
+#never finished making a table so this is just blank w data
+# table = TTY::Table.new(["Item", "Price"], [["a1", "a2"], ["b1", "b2"]])
+# p table.render(:ascii)

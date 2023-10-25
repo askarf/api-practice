7833554dae1622fe6e9a98be8a35a74f30380554
askarf <ayalakarf@gmail.com>
2023-10-25 15:38:28 -0400

    edited post to be what user imputs before hand

--- product_app.rb
+++ product_app.rb
@@ -1,30 +1,36 @@
 require "http"
 require "tty-table"
 
-all_products = HTTP.get("http://localhost:3000/products.json").parse
+# all_products = HTTP.get("http://localhost:3000/products.json").parse
 
-p "Welcome to my consignment shop!"
-p "would you like to see a specific item or a display of all of them??"
-input = gets.chomp
-if input == "specific"
-  puts "which id would you like to see?"
-  input_id = gets.chomp.to_i
-  specific_product = HTTP.get("http://localhost:3000/products/#{input_id}.json").parse
-  pp specific_product
-elsif input == "all"
-  pp all_products
-else
-  p "im sorry the options are specific or all, please try again"
-end
+# p "Welcome to my consignment shop!"
+# p "would you like to see a specific item or a display of all of them??"
+# input = gets.chomp
+# if input == "specific"
+#   puts "which id would you like to see?"
+#   input_id = gets.chomp.to_i
+#   specific_product = HTTP.get("http://localhost:3000/products/#{input_id}.json").parse
+#   pp specific_product
+# elsif input == "all"
+#   pp all_products
+# else
+#   p "im sorry the options are specific or all, please try again"
+# end
+
+puts "you are creating a new item"
+puts "enter name:"
+input_name = gets.chomp
+puts "enter price:"
+input_price = gets.chomp
+puts "enter image url:"
+input_url = gets.chomp
+puts "enter description:"
+input_description = gets.chomp
 
 response = HTTP.post("http://localhost:3000/products.json", :form => {
-                                                              :name => "Replive MuiMui Ballet Flats",
-                                                              :price => 40,
-                                                              :image_url => "www.tbd.com",
-                                                              :description => "Copy MuiMui pink ballet flats, size 7.5, with brown strap. never worn.",
+                                                              :name => input_name,
+                                                              :price => input_price,
+                                                              :image_url => input_url,
+                                                              :description => input_description,
 
                                                             })
-
-#never finished making a table so this is just blank w data
-# table = TTY::Table.new(["Item", "Price"], [["a1", "a2"], ["b1", "b2"]])
-# p table.render(:ascii)
